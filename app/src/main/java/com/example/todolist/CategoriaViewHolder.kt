ab07d59fd047ae2966f6da8fead9132730d7759d
Cristian PC <cristianpc3087@outlook.com>
2023-08-23 16:54:29 -0500

    Nuevo RecyclerView tareas

--- app/src/main/java/com/example/todolist/CategoriaViewHolder.kt
+++ app/src/main/java/com/example/todolist/CategoriaViewHolder.kt
@@ -1,14 +1,30 @@
 package com.example.todolist
 
+import android.graphics.Color
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoriaViewHolder(view: View):RecyclerView.ViewHolder(view) {
     private val tvCategoria:TextView = view.findViewById(R.id.tvCategoria)
     private val hr:View = view.findViewById(R.id.hr)
     fun render(categoriaTarea:CategoriaTareas){
-        tvCategoria.text = "Ejemplo"
+        when(categoriaTarea){
+            CategoriaTareas.Negocios->{
+                tvCategoria.text = "Negocios"
+                tvCategoria.setTextColor(Color.rgb(0, 128, 0))
+                hr.setBackgroundColor(Color.parseColor("#008000"))
+            }
+            CategoriaTareas.Personal->{
+                tvCategoria.text = "Personal"
+                hr.setBackgroundColor(Color.parseColor("#0000FF"))
+            }
+            CategoriaTareas.Otro->{
+                tvCategoria.text = "Otro"
+                hr.setBackgroundColor(ContextCompat.getColor(hr.context, R.color.my_color))
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.todolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvcategorias: RecyclerView
+    private val categorias = listOf(
+        CategoriaTareas.Negocios,
+        CategoriaTareas.Personal,
+        CategoriaTareas.Otro,
+    )
+
+    private lateinit var rvCategorias: RecyclerView
+    private lateinit var rvTareas: RecyclerView
+    private lateinit var categoriasAdapter:CategoriaAdapter
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +29,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initComponnet() {
-        rvcategorias = binding.rvCategorias
+        rvCategorias = binding.rvCategorias
+        rvTareas = binding.rvTareas
     }
     private fun initUI(){
-
+        categoriasAdapter= CategoriaAdapter(categorias)
+        rvCategorias.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategorias.adapter=categoriasAdapter
     }
 }
